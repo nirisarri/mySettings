7e0ad7faab4b68bc9be1f4cfb1a52548408da692
Nichola De Irisarri Boada <nboada@fbtestnet.com>
2018-05-16 08:18:21 -0700

    added vs2015 themes

---
+++ VS2015/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-a4faccd\src\posh-git.psd1'
+
+pushd 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools'    
+cmd /c "vsvars32.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
+popd
+write-host "`nVisual Studio 2015 Command Prompt variables set." -ForegroundColor Yellow
